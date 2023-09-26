81d05c041f7926b9a0cd5dacaf779b69fa0a1542
kutay-oksuzz <kutay_oksuzz@outlook.com>
2023-09-26 22:09:46 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require("request");
+
+const forecast = ({ latitude, longitude }, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=4175ac8a31e15a83f326cc1607fa2b84&query=${latitude},${longitude}&units=m`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Something is wrong!", undefined);
+    } else if (body.error) {
+      callback("blablabla");
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
